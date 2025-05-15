e1c5fcaba27f706b5e00918a4560d58b6a806473
Lance5057 <Lance5057@gmail.com>
2025-05-14 20:37:56 -0500

    Update CookingPotBlockEntity.java

--- src/main/java/vectorwing/farmersdelight/common/block/entity/CookingPotBlockEntity.java
+++ src/main/java/vectorwing/farmersdelight/common/block/entity/CookingPotBlockEntity.java
@@ -286,7 +286,9 @@ public class CookingPotBlockEntity extends SyncedBlockEntity implements MenuProv
 			if (recipe.value().matches(inventoryWrapper, level)) {
 				return Optional.of(recipe);
 			}
-			if (ItemStack.isSameItem(recipe.value().getResultItem(this.level.registryAccess()), getMeal())) {
+			if (ItemStack.isSameItem(
+					recipe.value().assemble(new RecipeWrapper(this.inventory), this.level.registryAccess()),
+					getMeal())) {
 				return Optional.empty();
 			}
 		}
@@ -325,7 +327,7 @@ public class CookingPotBlockEntity extends SyncedBlockEntity implements MenuProv
 
 	protected boolean canCook(CookingPotRecipe recipe) {
 		if (hasInput()) {
-			ItemStack resultStack = recipe.getResultItem(this.level.registryAccess());
+			ItemStack resultStack = recipe.assemble(new RecipeWrapper(this.inventory), this.level.registryAccess());
 			if (resultStack.isEmpty()) {
 				return false;
 			} else {
@@ -356,7 +358,7 @@ public class CookingPotBlockEntity extends SyncedBlockEntity implements MenuProv
 
 		cookTime = 0;
 		mealContainerStack = recipe.value().getOutputContainer();
-		ItemStack resultStack = recipe.value().getResultItem(this.level.registryAccess());
+		ItemStack resultStack = recipe.value().assemble(new RecipeWrapper(this.inventory), this.level.registryAccess());
 		ItemStack storedMealStack = inventory.getStackInSlot(MEAL_DISPLAY_SLOT);
 		if (storedMealStack.isEmpty()) {
 			inventory.setStackInSlot(MEAL_DISPLAY_SLOT, resultStack.copy());
