101e6d1d61ad48685dd5b23d0fd0a0be2ec5aefc
Lucas Barcellos <lucas.t.barcellos@gmail.com>
2026-02-04 19:52:10 -0300

    Add "sneak to place" config for pumpkin pie

--- src/main/java/vectorwing/farmersdelight/common/mixin/PlacePumpkinPieMixin.java
+++ src/main/java/vectorwing/farmersdelight/common/mixin/PlacePumpkinPieMixin.java
@@ -1,6 +1,7 @@
 package vectorwing.farmersdelight.common.mixin;
 
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.context.UseOnContext;
@@ -8,6 +9,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import vectorwing.farmersdelight.common.Configuration;
 import vectorwing.farmersdelight.common.registry.ModItems;
 
 @Mixin(Item.class)
@@ -17,10 +19,16 @@ public class PlacePumpkinPieMixin
 			method = "useOn",
 			at = @At("TAIL"),
 			cancellable = true)
-	private void usePumpkinPie(UseOnContext ctx, CallbackInfoReturnable<InteractionResult> cir)
-	{
-		if (ctx.getItemInHand().is(Items.PUMPKIN_PIE))
-		{
+	private void usePumpkinPie(UseOnContext ctx, CallbackInfoReturnable<InteractionResult> cir) {
+		if (!ctx.getItemInHand().is(Items.PUMPKIN_PIE))
+			return;
+
+		if (Configuration.PUMPKIN_PIE_SNEAK_TO_PLACE.get()) {
+			Player player = ctx.getPlayer();
+			if (player != null && player.isSecondaryUseActive()) {
+				cir.setReturnValue(ModItems.DEBUG_PUMPKIN_PIE.get().useOn(ctx));
+			}
+		} else {
 			cir.setReturnValue(ModItems.DEBUG_PUMPKIN_PIE.get().useOn(ctx));
 		}
 	}
