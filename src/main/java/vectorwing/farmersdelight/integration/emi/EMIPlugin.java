4e7e3409ea506bd46501767448559e1102f7c37a
Lucas Barcellos <lucas.t.barcellos@gmail.com>
2026-02-15 21:54:03 -0300

    Add special recipes to EMI

--- src/main/java/vectorwing/farmersdelight/integration/emi/EMIPlugin.java
+++ src/main/java/vectorwing/farmersdelight/integration/emi/EMIPlugin.java
@@ -3,17 +3,24 @@ package vectorwing.farmersdelight.integration.emi;
 import dev.emi.emi.api.EmiEntrypoint;
 import dev.emi.emi.api.EmiPlugin;
 import dev.emi.emi.api.EmiRegistry;
+import dev.emi.emi.api.recipe.EmiCraftingRecipe;
 import dev.emi.emi.api.stack.EmiIngredient;
 import dev.emi.emi.api.stack.EmiStack;
 import net.minecraft.client.Minecraft;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Items;
+import vectorwing.farmersdelight.FarmersDelight;
 import vectorwing.farmersdelight.common.crafting.CookingPotRecipe;
 import vectorwing.farmersdelight.common.crafting.CuttingBoardRecipe;
+import vectorwing.farmersdelight.common.registry.ModItems;
 import vectorwing.farmersdelight.common.registry.ModMenuTypes;
 import vectorwing.farmersdelight.common.registry.ModRecipeTypes;
 import vectorwing.farmersdelight.integration.emi.handler.CookingPotEmiRecipeHandler;
 import vectorwing.farmersdelight.integration.emi.recipe.CookingPotEmiRecipe;
 import vectorwing.farmersdelight.integration.emi.recipe.CuttingEmiRecipe;
 import vectorwing.farmersdelight.integration.emi.recipe.DecompositionEmiRecipe;
+
+import java.util.List;
 
 @EmiEntrypoint
 public class EMIPlugin implements EmiPlugin {
@@ -38,5 +45,14 @@ public class EMIPlugin implements EmiPlugin {
                     recipe.getRollableResults().stream().map(chanceResult -> EmiStack.of(chanceResult.getStack()).setChance(chanceResult.getChance())).toList()));
         }
         registry.addRecipe(new DecompositionEmiRecipe());
+
+        addSpecialRecipes(registry);
+    }
+
+    public void addSpecialRecipes(EmiRegistry registry) {
+        ResourceLocation doughRecipeId = new ResourceLocation(FarmersDelight.MODID, "wheat_dough_from_water");
+        if (registry.getRecipeManager().byKey(doughRecipeId).isPresent()) {
+            registry.addRecipe(new EmiCraftingRecipe(List.of(EmiStack.of(Items.WHEAT), EmiStack.of(Items.WATER_BUCKET)), EmiStack.of(ModItems.WHEAT_DOUGH.get()), doughRecipeId, true));
+        }
     }
 }
