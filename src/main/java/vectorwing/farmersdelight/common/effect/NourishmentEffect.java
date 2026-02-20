44ded622a6fe9e86937a163913fb8af6d514b64e
Lucas Barcellos <lucas.t.barcellos@gmail.com>
2026-02-20 13:19:14 -0300

    Clean up NourishmentEffect

--- src/main/java/vectorwing/farmersdelight/common/effect/NourishmentEffect.java
+++ src/main/java/vectorwing/farmersdelight/common/effect/NourishmentEffect.java
@@ -10,12 +10,12 @@ import net.minecraft.world.level.GameRules;
 public class NourishmentEffect extends MobEffect
 {
 	/**
-	 * This effect prevents hunger loss by constantly decreasing the exhaustion level.
-	 * If the player can spend saturation to heal damage, the effect halts to let them do so, until they can't any more.
-	 * This means players can grow hungry by healing damage, but no further than 1.5 points, allowing them to eat more and keep healing.
+	 * This effect prevents hunger loss by constantly setting the exhaustion level to zero.
+	 * If the player can spend saturation to heal damage, the effect pauses to let them do so.
+	 * Slow healing won't consume hunger, making it happen indefinitely. A mixin allows the player to always eat when under this effect to compensate.
 	 */
 	public NourishmentEffect() {
-		super(MobEffectCategory.BENEFICIAL, 15971072);
+		super(MobEffectCategory.BENEFICIAL, 0xF3B300);
 	}
 
 	public void applyEffectTick(LivingEntity entity, int amplifier) {
@@ -33,21 +33,6 @@ public class NourishmentEffect extends MobEffect
 				foodData.setExhaustion(0);
 			}
 		}
-//		if (!entity.getCommandSenderWorld().isClientSide && entity instanceof Player player) {
-//			FoodData foodData = player.getFoodData();
-//			boolean isPlayerHealingWithHunger =
-//					player.level().getGameRules().getBoolean(GameRules.RULE_NATURAL_REGENERATION)
-//							&& player.isHurt()
-////							&& foodData.getSaturationLevel() > 0.0F
-//							&& foodData.getFoodLevel() >= 18;
-//			if (!isPlayerHealingWithHunger) {
-//				float exhaustion = foodData.getExhaustionLevel();
-//				float reduction = Math.min(exhaustion, 4.0F);
-//				if (exhaustion > 0.0F) {
-//					player.causeFoodExhaustion(-reduction);
-//				}
-//			}
-//		}
 	}
 
 	@Override
