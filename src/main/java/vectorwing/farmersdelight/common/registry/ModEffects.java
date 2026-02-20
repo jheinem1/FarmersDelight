6e52bc9ff4d9cd385704b4b0a445a841addb77bb
Lucas Barcellos <lucas.t.barcellos@gmail.com>
2026-02-20 19:06:08 -0300

    Temp re-enable Comfort for addon compat

--- src/main/java/vectorwing/farmersdelight/common/registry/ModEffects.java
+++ src/main/java/vectorwing/farmersdelight/common/registry/ModEffects.java
@@ -6,11 +6,12 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import vectorwing.farmersdelight.FarmersDelight;
 import vectorwing.farmersdelight.common.effect.NourishmentEffect;
+import vectorwing.farmersdelight.common.effect.ComfortEffect;
 
 public class ModEffects
 {
 	public static final DeferredRegister<MobEffect> EFFECTS = DeferredRegister.create(ForgeRegistries.MOB_EFFECTS, FarmersDelight.MODID);
 
 	public static final RegistryObject<MobEffect> NOURISHMENT = EFFECTS.register("nourishment", NourishmentEffect::new);
-//	public static final RegistryObject<MobEffect> COMFORT = EFFECTS.register("comfort", ComfortEffect::new);
+	public static final RegistryObject<MobEffect> COMFORT = EFFECTS.register("comfort", ComfortEffect::new);
 }
