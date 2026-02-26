5893d2cc76a4284318fb63f9344d0bd0f90c33e5
Lucas Barcellos <lucas.t.barcellos@gmail.com>
2026-02-26 00:56:53 -0300

    Fix breaking and dropping behavior for vines

--- src/main/java/vectorwing/farmersdelight/common/block/HangingTomatoVineBlock.java
+++ src/main/java/vectorwing/farmersdelight/common/block/HangingTomatoVineBlock.java
@@ -5,19 +5,17 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.material.FluidState;
 import net.minecraftforge.registries.ForgeRegistries;
 import vectorwing.farmersdelight.common.Configuration;
 import vectorwing.farmersdelight.common.registry.ModBlocks;
 
-import javax.annotation.Nullable;
-
+@SuppressWarnings("deprecation")
 public class HangingTomatoVineBlock extends TomatoVineBlock
 {
 	public HangingTomatoVineBlock(Properties properties) {
@@ -38,12 +36,6 @@ public class HangingTomatoVineBlock extends TomatoVineBlock
 	}
 
 	@Override
-	public void playerDestroy(Level level, Player player, BlockPos pos, BlockState state, @Nullable BlockEntity blockEntity, ItemStack stack) {
-		super.playerDestroy(level, player, pos, state, blockEntity, stack);
-		placeRope(level, pos);
-	}
-
-	@Override
 	public void tick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
 		if (!state.canSurvive(level, pos)) {
 			level.destroyBlock(pos, true);
@@ -52,11 +44,19 @@ public class HangingTomatoVineBlock extends TomatoVineBlock
 	}
 
 	@Override
-	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-		builder.add(VINE_AGE);
+	public boolean onDestroyedByPlayer(BlockState state, Level level, BlockPos pos, Player player, boolean willHarvest, FluidState fluid) {
+		this.playerWillDestroy(level, pos, state, player);
+		return placeRope(level, pos);
 	}
 
-	public static void placeRope(Level level, BlockPos pos) {
+	public void onRemove(BlockState state, Level level, BlockPos pos, BlockState newState, boolean movedByPiston) {
+		super.onRemove(state, level, pos, newState, movedByPiston);
+		if (!movedByPiston && !state.is(newState.getBlock())) {
+			placeRope(level, pos);
+		}
+	}
+
+	public static boolean placeRope(Level level, BlockPos pos) {
 		Block configuredRopeBlock = ForgeRegistries.BLOCKS.getValue(new ResourceLocation(Configuration.DEFAULT_TOMATO_VINE_ROPE.get()));
 		if (configuredRopeBlock == null) {
 			configuredRopeBlock = ModBlocks.ROPE.get();
@@ -65,6 +65,11 @@ public class HangingTomatoVineBlock extends TomatoVineBlock
 				? RopeBlock.getStateWithConnections(ModBlocks.ROPE.get().defaultBlockState(), level, pos)
 				: configuredRopeBlock.defaultBlockState();
 
-		level.setBlockAndUpdate(pos, finalRopeState);
+		return level.setBlock(pos, finalRopeState, level.isClientSide ? 11 : 3);
+	}
+
+	@Override
+	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+		builder.add(VINE_AGE);
 	}
 }
